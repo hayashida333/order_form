14feee28337b768fb557a31fe2b3260f90a9a4b0
hayashida <kenyalintian@gmail.com>
2025-05-20 22:31:06 +0900

    rubocop-todoの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  root "top#index"
+  root 'top#index'
   resources :orders, only: %i[new create] do
     collection do
       post :confirm
       get :complete
-      get "index", to: "pages#index"
+      get 'index', to: 'pages#index'
     end
   end
   resources :order_histories, only: %i[index]
